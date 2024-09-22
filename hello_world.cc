67a1b59c7342597b1eb5b73737c998bb5d7544be
AlejandroSocas <alu0101725055@ull.edu.es>
2024-09-22 18:50:44 +0000

    Subida del archivo hello_world

---
+++ hello_world.cc
@@ -0,0 +1,19 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Alejandro Socas González
+ * @date 21 Sep 2024
+ * @brief The classical 'Hello World' first program in any language
+ *
+ * @see http://www.cplusplus.com/doc/tutorial/program_structure/
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello_World!" << std::endl;
+	return 0;
+}
